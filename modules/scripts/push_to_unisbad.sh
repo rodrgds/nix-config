bad6b2cc580fc6ceb81df599c6c60eb57914a6b8
Rodrigo Dias <kraktoos@gmail.com>
2026-02-02 12:13:17 +0000

    Add push_to_unisbad script for automated file transfer and commit

---
+++ modules/scripts/push_to_unisbad.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+SOURCE="/home/rgo/JDSystem/30-39 Projects/32 Uni's Bad/32.11 Public-Facing"
+TARGET="/home/rgo/dev/unis-bad/content"
+mkdir -p "$TARGET"
+cp -r "$SOURCE"/* "$TARGET/"
+cd /home/rgo/dev/unis-bad
+git add -A
+git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
