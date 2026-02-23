88303d5ef490adf3045b5da9fd7169e6bf3f1152
Rodrigo Dias <me@rgo.pt>
2026-02-23 14:52:52 +0000

    macos virtual box fixes

--- modules/apps/virtualbox/default.nix
+++ modules/apps/virtualbox/default.nix
@@ -25,6 +25,11 @@ in
       # Darwin: Install via Homebrew cask
       (lib.optionalAttrs isDarwin {
         homebrew.casks = [ "virtualbox" ];
+
+        # ARM-specific VirtualBox configuration for Apple Silicon Macs
+        # Enable x86 emulation on ARM (VirtualBox needs this for Intel VMs on M1/M2/M3/M4)
+        # This command enables VirtualBox to run x86/x64 VMs on Apple Silicon
+        # Run manually after installation: VBoxManage setextradata global "VBoxInternal2/EnableX86OnArm" 1
       })
     ]
   );
