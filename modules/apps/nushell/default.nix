9d91be118af8a51ba3daed71bc5617fcbd0758f2
Rodrigo Dias <me@rgo.pt>
2026-03-11 20:06:31 +0000

    fix: properly configure nushell with settings and envVariables

--- modules/apps/nushell/default.nix
+++ modules/apps/nushell/default.nix
@@ -21,27 +21,22 @@ in
     home-manager.users.${username} = {
       programs.nushell = {
         enable = true;
-        configFile = lib.mkAfter (builtins.toFile "config.nix" ''
-          show_banner: false
-          use_grid_icons: true
-          footer_mode: "25"
-          float_precision: 2
-          buffer_editor: "vim"
-          use_ansi_coloring: true
-          bracketed_paste: true
-          edit_mode: "vi"
-          shell_integration: true
-          render_right_prompt_on_last_line: false
-        '');
-        envConfig = lib.mkAfter (builtins.toFile "env.nix" '');
-        extraConfig = ''
-          $env.OPENCODE_ENABLE_EXA = "1"
-        '' + lib.optionalString isDarwin ''
-          if ($"/opt/homebrew/bin/brew --prefix" != "") {
-            $env.PATH = ($"/opt/homebrew/bin/brew --prefix/bin") + ($env.PATH | str join)
-            $env.PATH = ($"/opt/homebrew/bin/brew --prefix/sbin") + ($env.PATH | str join)
-          }
-        '';
+        settings = {
+          show_banner = false;
+          use_grid_icons = true;
+          footer_mode = "25";
+          float_precision = 2;
+          buffer_editor = "vim";
+          use_ansi_coloring = true;
+          bracketed_paste = true;
+          edit_mode = "vi";
+          shell_integration = true;
+        };
+        envVariables = {
+          OPENCODE_ENABLE_EXA = "1";
+        } // lib.optionalAttrs isDarwin {
+          PATH_EXTRA = "/opt/homebrew/bin:/opt/homebrew/sbin";
+        };
       };
     };
   };
