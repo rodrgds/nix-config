6560d8d04a36b4d4192e8268d33dbb1e43136354
Rodrigo Dias <kraktoos@gmail.com>
2026-03-11 21:48:39 +0000

    fix: remove unnecessary shellAliases from environment configuration

--- modules/core/environment/default.nix
+++ modules/core/environment/default.nix
@@ -19,13 +19,5 @@ in
       ANDROID_HOME = "$HOME/.android/sdk";
       ANDROID_SDK_ROOT = "$HOME/.android/sdk";
     };
-
-    environment.shellAliases = {
-      "copy" = "xclip -selection clipboard";
-      "v" = "nvim";
-      "glog" = "git log --oneline --graph --decorate --all";
-      "ll" = "ls -laFh";
-      "rescrobbled-logs" = "journalctl --user -u rescrobbled.service -f";
-    };
   };
 }
