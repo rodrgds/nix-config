0587fed5ecde9b7e9b2ee44cb42f068c7cc61de1
Rodrigo Dias <kraktoos@gmail.com>
2026-03-11 21:47:23 +0000

    fix: correct formatting of environmentVariables in nushell config

--- modules/apps/nushell/default.nix
+++ modules/apps/nushell/default.nix
@@ -30,15 +30,12 @@ in
           edit_mode = "vi";
           shell_integration = true;
         };
-        environmentVariables =
-          {
-            OPENCODE_ENABLE_EXA = "1";
-          }
-            lib.optionalAttrs
-            isDarwin
-            {
-              PATH_EXTRA = "/opt/homebrew/bin:/opt/homebrew/sbin";
-            };
+        environmentVariables = {
+          OPENCODE_ENABLE_EXA = "1";
+        }
+        // lib.optionalAttrs isDarwin {
+          PATH_EXTRA = "/opt/homebrew/bin:/opt/homebrew/sbin";
+        };
       };
     };
   };
