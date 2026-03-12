0ed8eb348bbb6030a1ecb781312b3e93663cffd5
Rodrigo Dias <kraktoos@gmail.com>
2026-03-12 20:36:57 +0000

    fix: update rgo-openclaw device ID in Syncthing configuration

--- modules/apps/syncthing/default.nix
+++ modules/apps/syncthing/default.nix
@@ -40,7 +40,7 @@ in
                 id = "ZEVRZIA-4O22UC5-5XPMEAD-HGW3WEZ-F7J6ZTU-USJCCNC-WAB72VT-QIOL6QV";
               };
               rgo-openclaw = {
-                id = "MH2IIBT-WT5WC3P-SLHCOFN-U5VAOEX-JGVK33L-PIBBT5Y-PWGXBY4-NVQBZA5";
+                id = "X3J2265-7PNVAYL-NHNAAJC-OZLWWGQ-QIGL7X2-HIVIDP6-PGEMPIU-LQIFFQB";
               };
             };
           };
