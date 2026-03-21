6b64188a70666c770942d72808b69e42b028ca7a
Rodrigo Dias <kraktoos@gmail.com>
2026-03-21 18:09:20 +0000

    feat(android-studio): add LD_LIBRARY_PATH for Android Studio environment

--- modules/apps/android-studio/default.nix
+++ modules/apps/android-studio/default.nix
@@ -28,6 +28,7 @@ in
           CAPACITOR_ANDROID_STUDIO_PATH = "${pkgs.android-studio}/bin/android-studio";
           VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.x86_64.json";
           ANDROID_EMULATOR_USE_SYSTEM_LIBS = "1";
+          LD_LIBRARY_PATH = "${pkgs.libglvnd}/lib:/run/opengl-driver/lib:/run/opengl-driver-32/lib";
         };
 
         nixpkgs.config.android_sdk.accept_license = true;
