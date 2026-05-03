876d77100680ae42362b52ea1c8783ebaefd8e7b
Rodrigo Dias <kraktoos@gmail.com>
2026-05-03 17:43:14 +0100

    Generation 254

--- modules/apps/pi/default.nix
+++ modules/apps/pi/default.nix
@@ -18,9 +18,21 @@ in
   config = lib.mkIf cfg.enable {
     apps.nodejs.enable = true;
 
+    # Essential CLI tools for pi
+    environment.systemPackages = [
+      pkgs.ripgrep
+      pkgs.fd
+    ];
+
+    # Keep GUI tools able to resolve them
     home-manager.users.${username} =
-      { lib, ... }:
+      { lib, pkgs, ... }:
       {
+        home.packages = [
+          pkgs.ripgrep
+          pkgs.fd
+        ];
+
         home.sessionPath = [ "$HOME/${installDir}/bin" ];
 
         home.activation.installPiCli = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
@@ -31,8 +43,10 @@ in
           ${pkgs.nodejs}/bin/npm install --global --prefix "$INSTALL_ROOT" ${packageName}
         '';
 
-        home.file.".pi/agent/settings.json".text = builtins.toJSON {
-          npmCommand = [ "${pkgs.nodejs}/bin/npm" ];
+        home.file = {
+          ".pi/agent/settings.json".text = builtins.toJSON {
+            npmCommand = [ "${pkgs.nodejs}/bin/npm" ];
+          };
         };
       };
   };
