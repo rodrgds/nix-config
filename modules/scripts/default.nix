cae68222d23b47cdeb196807c34e06ae63a73a63
Rodrigo Dias <kraktoos@gmail.com>
2026-06-08 11:23:02 +0100

    refactor: remove legacy and redundant secret management aliases

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -62,19 +62,6 @@ let
     "glog" = "git log --oneline --graph --decorate --all";
     "ll" = "ls -la";
     "rebuild" = "bun ${homeDir}/.config/home/tools/rebuild-wizard/rebuild.ts";
-
-    # Legacy aliases (default to main secrets.yaml)
-    # "encrypt_secrets" = "bash ${homeDir}/.config/home/modules/scripts/secrets.sh encrypt secrets";
-    # "decrypt_secrets" = "bash ${homeDir}/.config/home/modules/scripts/secrets.sh decrypt secrets";
-    # "decrypt_to_file" =
-    #   "bash ${homeDir}/.config/home/modules/scripts/secrets.sh decrypt-to-file secrets";
-    # "edit_secrets" = "bash ${homeDir}/.config/home/modules/scripts/secrets.sh edit secrets";
-
-    # New flexible aliases - usage: decrypt secrets | decrypt vps-secrets
-    "decrypt" = "bash ${homeDir}/.config/home/modules/scripts/secrets.sh decrypt";
-    "encrypt" = "bash ${homeDir}/.config/home/modules/scripts/secrets.sh encrypt";
-    "decrypt-file" = "bash ${homeDir}/.config/home/modules/scripts/secrets.sh decrypt-to-file";
-    "edit-encrypted" = "bash ${homeDir}/.config/home/modules/scripts/secrets.sh edit";
   };
 
   # Linux-specific aliases
