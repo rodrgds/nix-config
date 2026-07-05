3b1bb01ea734841d59666ad2253b2cb40b251f81
Rodrigo Dias <me@rgo.pt>
2026-07-05 22:58:58 +0100

    deploy(openpost): pin v1.0.33

--- hosts/rgo-vps/default.nix
+++ hosts/rgo-vps/default.nix
@@ -84,7 +84,7 @@
   vps.openpost = {
     enable = true;
     edition = "cloud";
-    image = "ghcr.io/rodrgds/openpost:v1.0.30";
+    image = "ghcr.io/rodrgds/openpost:v1.0.33";
   };
   vps.unprompted = {
     enable = true;
